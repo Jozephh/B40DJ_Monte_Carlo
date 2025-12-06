6656850a9eb151b393211994baaedffc4d211438
Joe Bradshaw <joethebradshaw@gmail.com>
2025-12-06 19:57:32 +0000

    new variables added

--- monte.py
+++ monte.py
@@ -10,7 +10,7 @@ data3 = pd.read_excel(r"C:\Users\joe\OneDrive - Heriot-Watt University\Uni_Cloud
 data4 = pd.read_excel(r"C:\Users\joe\OneDrive - Heriot-Watt University\Uni_Cloud\Year 4\B40DJ_Sus_Mgmt_&_Process_Econ\Assignment 2\Assignment 2.xlsm", sheet_name=4)
 
 # -----------------------------
-# 1. BASE CASE (USD)
+# 1. BASE CASE
 # -----------------------------
 
 N_SIM = 10000
@@ -22,24 +22,36 @@ project_life = 20
 discount_rate = data4.iloc[1,13]
 discount_rate = int(discount_rate)
 
+FCI_base   = data4.iloc[2,4]   
+FCI_base = int(FCI_base)       # from Year 0
 
-FCI_base   = 37_839_296          # from Year 0
-Revenues_base = 225_717_722      # from table
-OPEX_base  = 207_159_058        # from table
-Sigma_Ce   = 6_300_249           # from table
+Revenues_base = data4.iloc[17,3]   
+Revenues_base = int(Revenues_base)   # from table
+
+OPEX_base  = data4.iloc[18,3]        # from table
+OPEX_base = int(OPEX_base)
+
+Sigma_Ce = data4.iloc[2,3]           # from table
+Sigma_Ce = int(Sigma_Ce)
+
+plant_cap_base = data3.iloc[0,2]    # plant capacity factor 
+plant_cap_base = int(plant_cap_base)
+
 scrap_base = 0.10 * Sigma_Ce     # 10 % of equipment cost
 
 # -----------------------------
 # 2. UNCERTAINTY RANGES
 # -----------------------------
-rev_low, rev_high   = 0.8, 1.2   # ±20% on revenue
-opex_low, opex_high = 0.8, 1.2   # ±20% on OPEX
-capex_low, capex_high = 0.95, 1.05 # ±40% on FCI and scrap
+rev_low, rev_high   = 0.6, 1.4   # ±40% on revenue
+opex_low, opex_high = 0.6, 1.4  # ±40% on OPEX
+capex_low, capex_high = 0.95, 1.05 # ±5% on FCI and scrap
+plant_cap_low, plant_cap_high = 0.8, 1 # -20% on plant capacity could be due to maintenance, emergency shutdown etc
 
 # Sample random multipliers
 rev_factor   = np.random.uniform(rev_low, rev_high,   N_SIM)
 opex_factor  = np.random.uniform(opex_low, opex_high, N_SIM)
 capex_factor = np.random.uniform(capex_low, capex_high, N_SIM)
+plant_cap_factor = np.random.uniform(plant_cap_low, plant_cap_high, N_SIM)
 
 NPV = np.zeros(N_SIM)
 
@@ -52,6 +64,7 @@ for i in range(N_SIM):
     OPEX = OPEX_base * opex_factor[i]
     FCI  = FCI_base * capex_factor[i]
     scrap = scrap_base * capex_factor[i]
+    pcap = plant_cap_base * plant_cap_factor[i]
 
     CF = np.zeros(project_life + 1)
     CF[0] = -FCI
@@ -71,18 +84,20 @@ mean_NPV = np.mean(NPV)
 p5 = np.percentile(NPV, 5)
 p95 = np.percentile(NPV, 95)
 prob_positive = np.mean(NPV > 0)
-corr_rev   = np.corrcoef(rev_factor,   NPV)[0,1]
-corr_opex  = np.corrcoef(opex_factor,  NPV)[0,1]
+corr_rev   = np.corrcoef(rev_factor, NPV)[0,1]
+corr_opex  = np.corrcoef(opex_factor, NPV)[0,1]
 corr_capex = np.corrcoef(capex_factor, NPV)[0,1]
+corr_pcap = np.corrcoef(plant_cap_factor, NPV)[0,1]
 
 print(f"Mean NPV      = ${mean_NPV/1e6:.2f} M")
 print(f"5th percentile = ${p5/1e6:.2f} M")
 print(f"95th percentile= ${p95/1e6:.2f} M")
-print(f"P(NPV > 0)     = {prob_positive*100:.1f} %")
+print(f"P(NPV > 0) = {prob_positive*100:.1f} %")
 print("Correlation with NPV:")
 print(f"Revenue factor: {corr_rev:.3f}")
-print(f"OPEX factor:    {corr_opex:.3f}")
-print(f"CAPEX factor:   {corr_capex:.3f}")
+print(f"OPEX factor: {corr_opex:.3f}")
+print(f"CAPEX factor: {corr_capex:.3f}")
+print(f"pcap factor: {corr_pcap:.3f}")
 
 # -----------------------------
 # 5. PLOTS
